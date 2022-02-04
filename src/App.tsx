f37a5ba0e8807ecfa49e90f6f4bf78245c2e13ca
aeuna <4294psy@naver.com>
2022-02-04 23:45:18 +0900

    slider part with AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -9,43 +9,68 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #778beb, #546de5);
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 300px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 20px;
   position: absolute;
   top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  initial: {
+  entry: (back: boolean) => ({
+    x: back ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 0.5,
+    },
   },
-  leaving: {
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
     opacity: 0,
     scale: 0,
-    y: 50,
-  },
+    transition: {
+      duration: 0.5,
+    },
+  }),
 };
 
 function App() {
-  const [showing, setShowing] = useState<Boolean>(false);
-  const toggleBtn = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
 
   return (
     <Wrapper>
-      <button onClick={toggleBtn}>Click</button>
-      <AnimatePresence>{showing ? <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving" /> : null}</AnimatePresence>
+      <AnimatePresence custom={back}>
+        <Box custom={back} variants={boxVariants} initial="entry" animate="center" exit="exit" key={visible}>
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <button onClick={nextPlease}>Next</button>
+      <button onClick={prevPlease}>Prev</button>
     </Wrapper>
   );
 }
